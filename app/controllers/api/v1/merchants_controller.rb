8542d329165215e58efd27e953c8485de2d0f319
Annie Pulzone <ajpulzone@gmail.com>
2022-12-06 19:04:46 -0800

    Add tests and functionality to find one merchant based on ID

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+  # private
+  #   def merchant_params
+  #     params.require(:merchant).permit(:id, :name)
+  #   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -17,9 +17,29 @@ describe "Merchants API" do
 
       expect(merchant).to have_key(:type)
       expect(merchant[:type]).to be_a(String)
+      
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to eq("merchant")
 
       expect((merchant)[:attributes]).to have_key(:name)
       expect((merchant)[:attributes][:name]).to be_a(String)
     end
-  end
+  end 
+
+  it "can get one merchant by their id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to eq("#{id}")
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+    
+  end 
 end
